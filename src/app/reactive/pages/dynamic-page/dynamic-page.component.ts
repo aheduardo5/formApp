bf4df4de5b6cb2fe362fa63e5b9bc94cfd0c7fec
Eduardo Aguilar <aheduardo5@gmail.com>
2024-03-19 16:31:07 -0600

    routing has been added

---
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dynamic-page',
+  templateUrl: './dynamic-page.component.html',
+})
+export class DynamicPageComponent {}
